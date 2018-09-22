3b4364c152ea6dde01509fe59439eca0c55b9695
Saib Ahmed <saibahmed1512@gmail.com>
2018-09-22 21:13:16 +0530

    chnaged 92 to 90

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app =express();
 
 app.get("/",function (req,res){
-    res.json({response:"96"});
+    res.json({response:"90"});
 })
 const port=process.env.PORT||5000;
 app.listen(port,()=>{
     console.log("server is running at",port);
 });
+
